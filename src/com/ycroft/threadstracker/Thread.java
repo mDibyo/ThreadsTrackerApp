387f18b6bf2f5bc4ccccbf888f03cbacb579146b
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-05-30 16:34:18 +0400

    Added setters and getters

--- src/com/ycroft/threadstracker/Thread.java
+++ src/com/ycroft/threadstracker/Thread.java
@@ -17,5 +17,45 @@ public class Thread {
 		mDone = done;
 		mImportance = importance;
 	}
+	
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+	
+	public String getTitle() {
+		return mTitle;
+	}
+	
+	public void setDeadline(Date deadline) {
+		mDeadline = deadline;
+	}
+	
+	public Date getDeadline() {
+		return mDeadline;
+	}
+	
+	public void setCategory(Category category) {
+		mCategory = category;
+	}
+	
+	public Category getCategory() {
+		return mCategory;
+	}
+	
+	public void setDone(boolean done) {
+		mDone = done;
+	}
+	
+	public boolean isDone() {
+		return mDone;
+	}
+	
+	public void setImportance(int importance) {
+		mImportance = importance;
+	}
+	
+	public int getImportance() {
+		return mImportance;
+	}
 
 }
