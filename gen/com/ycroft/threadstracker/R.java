da005304257ea37bbc590c9d47356e5977d111b5
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-09 18:17:45 -0700

    Layout of thread updated with name

--- gen/com/ycroft/threadstracker/R.java
+++ gen/com/ycroft/threadstracker/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1042,6 +1042,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int thread_name=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1099,7 +1100,7 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_fragment=0x7f030017;
         public static final int activity_threads=0x7f030018;
-        public static final int fragment_threads=0x7f030019;
+        public static final int fragment_thread=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
@@ -1149,6 +1150,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int thread_name_hint=0x7f0a0010;
     }
     public static final class style {
         /** 
